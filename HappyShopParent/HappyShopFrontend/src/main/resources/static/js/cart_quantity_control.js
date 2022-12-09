26bc1ce4b1d8b01e9845a094b5699ce6c524a444
huantran2605 <huantran2605@gmail.com>
2022-12-09 11:47:11 +0700

    code quantity_coltrol cart

---
+++ HappyShopParent/HappyShopFrontend/src/main/resources/static/js/cart_quantity_control.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	var e;
+	$(".link_Minus").on("click", function(e){
+		e.preventDefault();
+		productId = $(this).attr("pid");
+		quantity = $("#p" + productId);
+		e = quantity
+		quantityValue = parseInt(quantity.val()) - 1;
+		message = $("#m" + productId);
+		if(quantityValue < 1){
+			message.text("The min quatity is 1");
+		}else{
+			message.text("");
+			quantity.val(quantityValue);			
+		}
+	});
+	
+	$(".link_Plus").on("click", function(e){
+		e.preventDefault();
+		productId = $(this).attr("pid");
+		quantity = $("#p" + productId);
+		quantityValue = parseInt(quantity.val()) + 1;
+		maxQuantityValue =  parseInt(quantity.attr("max"));
+		if (quantityValue <= maxQuantityValue) {
+			quantity.val(quantityValue);
+		}
+	});
+	checkValueQuantity();
+	
+});
+
+function checkValueQuantity(){
+	$(".quantity_Input").on("click", function(){
+		productIdString = $(this).attr("id");
+		productId = parseInt(productIdString.substring(1, productIdString.length));
+		message = $("#m" + productId);
+		message.text("");
+		maxQuantityValue =  parseInt($(this).attr("max"));
+		$(this).on("change", function(){
+			quantityValue = parseInt($(this).val());
+			if(quantityValue > maxQuantityValue){
+				$(this).val(maxQuantityValue);
+			}
+			else if(quantityValue < 1){
+				$(this).val(1);
+				message.text("The min quatity is 1");
+			}
+		});
+				
+	});
+	
+	
+	
+}
